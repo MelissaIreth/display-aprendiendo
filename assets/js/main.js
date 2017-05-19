1b3992397fec3349fa582a7b2ba10aa2adb2ca09
Melissa <melissa.pacheco.martinez@gmail.com>
2017-05-19 10:43:28 -0400

    display

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,8 @@
-alert("Hola mundo");
+(function displayNone(){
+	var cajas = Array.from(document.getElementsByClassName('caja'));
+	cajas.forEach(function(e){
+		e.addEventListener("click", function(){
+			this.classList.toggle("no");
+		})
+	})
+})()
